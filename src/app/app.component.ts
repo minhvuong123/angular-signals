f084ca0a91bf338e543f3a294bc9188ede947228
minhvuong123 <minhvuong12222@gmail.com>
2024-03-24 02:04:44 +0700

    angular signale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { DefaultComponent } from './default/default.component';
+import { SignalsComponent } from './signals/signals.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [DefaultComponent, SignalsComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'angular-signal';
